f7a405349a8bc08b95ca54ed4ca00b3c697f88ea
Evgenij <eugeniy.karyakin@thinfilmnfc.com>
2019-07-08 16:17:06 +0300

    JSD: Setup environment: Setup webpack for development

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import _ from 'lodash';
+import singletonData from './patterns/singleton';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'WebPack'], ' ');
-  return element;
-}
-document.body.appendChild(component());
-
+console.log(singletonData);
